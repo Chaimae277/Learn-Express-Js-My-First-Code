570a1da1591dbac60bce978e35ebbdbd0eb90b9e
Your Name <you@example.com>
2024-12-20 18:23:21 +0100

    basic rest api with express js

---
+++ build-rest-api/index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+port = 8000;
+
+// Middleware to parse JSON
+app.use(express.json()); //Built In
+
+// Students Data
+const students = [
+  {id: 1, name: "Mohammed", note: 20},
+  {id: 2, name: "Karim", note: 15},
+  {id: 3, name: "Chaimae", note: 18},
+  {id: 4, name: "Sara", note: 11},
+  {id: 5, name: "Nouh", note: 19},
+  {id: 6, name: "Houd", note: 17},
+  {id: 7, name: "Oumaima", note: 13}
+]
+
+// Get All Students
+app.get('/', (req, res) => {
+  res.send(students)
+})
+
+// Get One Student By ID
+app.get('/:studentId', (req, res) => {
+  const {studentId} = req.params;
+  const foundStudent = students.find((item) => item.id === Number(studentId))
+  if(foundStudent) {
+    // res.send(foundStudent)
+    res.status(200).json({
+      message : "Found Student !",
+      data : foundStudent
+    })
+  } else {
+    // const foundStudent = students.find((item) => item.id === Number(studentId))
+    res.status(500).json({
+      message : `Student with ID ${studentId} not found`
+    })
+  }
+})
+
+// Delete One Student By ID
+app.delete('/:studentId', (req, res) => {
+  // Check Student if exist
+  const {studentId} = req.params;
+  const foundStudent = students.find((item) => item.id === Number(studentId));
+  if(!foundStudent) {
+    res.status(500).json({
+      message : `Student with ID ${studentId} not found`
+    })
+  } else {
+    const newStudentData = students.filter((item) => item.id !== Number(studentId));
+    if(newStudentData){
+      res.status(200).json({
+        data : newStudentData
+      })
+    }
+  }
+})
+
+app.listen(port, () => {
+  console.log('Connection with success');
+})
